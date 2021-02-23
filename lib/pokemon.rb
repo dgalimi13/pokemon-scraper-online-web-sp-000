9c7412f48fcad032e4c52344d23d90d7507a1404
danny galimi <dannygalimi13@aim.com>
2021-02-23 21:00:58 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
   end
   
   def self.find(id,db)
-    sql = "SELECT * FROM songs WHERE id = ?"
+    sql = "SELECT * FROM pokemon WHERE id = ?"
     result = db.execute(sql, id)[0]
     Pokemon.new(result[0], result[1], result[2])
   end

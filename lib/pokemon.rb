61aaefbd8963ff30509ccb9a7e88b1e50c1db55a
danny galimi <dannygalimi13@aim.com>
2021-02-23 20:45:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,7 +14,7 @@ class Pokemon
       VALUES (?, ?,?)
     SQL
  
-    DB[:conn].execute(sql, self.name, self.type, self.db)
+    DB[:conn].execute(sql, self.name, self.type, self.@db)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
   

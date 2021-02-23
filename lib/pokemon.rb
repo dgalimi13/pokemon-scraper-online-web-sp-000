14d00a7210f61db02c79ed0f34f5b76378b68e5d
danny galimi <dannygalimi13@aim.com>
2021-02-23 20:59:34 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
     db.execute(sql, name, type)
   end
   
-  def self.find(id)
+  def self.find(id,db)
     sql = "SELECT * FROM songs WHERE id = ?"
     result = DB[:conn].execute(sql, name)[0]
     Song.new(result[0], result[1], result[2])

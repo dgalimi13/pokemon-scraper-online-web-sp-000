65f0d9d1867dcc83e1603bcd0ec23d8cb79a2ba1
danny galimi <dannygalimi13@aim.com>
2021-02-23 21:55:55 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,7 +23,7 @@ class Pokemon
     FROM pokemon 
     WHERE id = ?
     SQL
-    result = db.execute(sql, id)
+    result = db.execute(sql, id)[0]
     Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
   end
   

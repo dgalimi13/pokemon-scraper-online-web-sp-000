0172f1ec9f20a9c1c8054027fb1a5521b6dd04c2
danny galimi <dannygalimi13@aim.com>
2021-02-23 19:47:16 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,12 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db 
   
+  def initialze(:id,:name,:type,:db)
+    @id = id 
+    @name = name 
+    @type = type
+    @db = db
+  end
   
   
 end
